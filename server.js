d202298af96c5ec9d528c6e099e9dba5122c835e
Mesum Abbas <i.mesumabbas#@gmail.com>
2026-04-01 23:23:15 +0500

    Updated

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(compression());
 app.use(cors({
   origin: true, // Allow all origins in development
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
 }));
 const apiLimiter = rateLimit({
